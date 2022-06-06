0ab1169a049dc45d36646111097c4606715e28ae
Toronto2022 <gala.april.ca@gmail.com>
2022-06-06 18:46:34 -0400

    added all media queries

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,30 @@ $(function () {
     arrows: true,
     infinite: false,
     prevArrow: '<button class="slick-prev" type="button"><svg> <use xlink: href = "images/sprite.svg#slider-arrow" > < /use> </svg></button > ',
-    nextArrow: '<button class="slick-next" type="button"><svg> <use xlink: href = "images/sprite.svg#slider-arrow" > < /use> </svg></button > '
+    nextArrow: '<button class="slick-next" type="button"><svg> <use xlink: href = "images/sprite.svg#slider-arrow" > < /use> </svg></button > ',
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        dots: false,
+      }
+    }]
   });
+
+});
+
+
+$(window).on('load resize', function () {
+  if ($(window).width() < 576) {
+    $('.best-restaurants__items').slick({
+      centerMode: false,
+      prevArrow: false,
+      nextArrow: false,
+      dots: true,
+      infinite: true,
+      speed: 100,
+      slidesToShow: 1
+    });
+  } else {
+    $(".best-restaurants__items.slick-initialized").slick("unslick");
+  }
 });
