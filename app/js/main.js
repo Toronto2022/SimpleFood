2d0d214385859bb136368cf2e728cab98e62050d
Toronto2022 <gala.april.ca@gmail.com>
2022-07-13 20:11:57 -0400

    added media queries

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,27 @@
-var mixer = mixitup('.burgers');
 $(function () {
 
   $('.product-catalog__name,.product-catalog__number').styler();
 
-  $('.product-catalog-price__input').ionRangeSlider({
+  $('.catalog-price__input').ionRangeSlider({
     type: "double",
     onStart: function (data) {
-      $('.product-catalog-price__from').text(data.from);
-      $('.product-catalog-price__to').text(data.to);
+      $('.catalog-price__from').text(data.from);
+      $('.catalog-price__to').text(data.to);
     },
     onChange: function (data) {
-      $('.product-catalog-price__from').text(data.from);
-      $('.product-catalog-price__to').text(data.to);
+      $('.catalog-price__from').text(data.from);
+      $('.catalog-price__to').text(data.to);
     },
-  })
+  });
+
+  $('.burger,.mobile-menu ').on('click', function () {
+    $('.mobile-menu,.overlay').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+  $('.product-catalog__btn,.mobile-catalog').on('click', function () {
+    $('.mobile-catalog,.overlay').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
 
   $('.comments__inner').slick({
     dots: true,
@@ -34,7 +42,7 @@ $(function () {
 
 $(window).on('load resize', function () {
   if ($(window).width() < 576) {
-    $('.best-restaurants__items').slick({
+    $('.best-restaurants__items,.discounts__items').slick({
       centerMode: false,
       prevArrow: false,
       nextArrow: false,
@@ -44,11 +52,10 @@ $(window).on('load resize', function () {
       slidesToShow: 1
     });
   } else {
-    $(".best-restaurants__items.slick-initialized").slick("unslick");
+    $(".best-restaurants__items.slick-initialized,.discounts__items").slick("unslick");
   }
 });
 
-$('.burger,.mobile-menu ').on('click', function () {
-  $('.mobile-menu,.overlay').toggleClass('active');
-  $('body').toggleClass('lock');
-});
+
+
+var mixer = mixitup('.popular-categories__burgers');
